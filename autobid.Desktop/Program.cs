a77d3aa8af429b3a16e362875a0fe467342b851b
Jakob-Hjort <jakob.hjort@gmail.com>
2025-09-25 13:05:57 +0200

    lavet noget GUI samt mockup til login,

--- autobid.Desktop/Program.cs
+++ autobid.Desktop/Program.cs
@@ -1,23 +1,13 @@
-﻿using System;
+﻿using Avalonia;
+using autobid;                // <- vigtigt: App-klassen ligger i UI-projektet
 
-using Avalonia;
-
-namespace autobid.Desktop;
-
-class Program
+internal static class Program
 {
-    // Initialization code. Don't use any Avalonia, third-party APIs or any
-    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-    // yet and stuff might break.
-    [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args) =>
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
 
-    // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
-            .UsePlatformDetect()
-            .WithInterFont()
-            .LogToTrace();
-
+        => AppBuilder.Configure<App>()      // <- autobid.App
+                     .UsePlatformDetect()
+                     .LogToTrace();
 }
